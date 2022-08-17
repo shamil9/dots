f1ea600c5419beb1c2823b038df5b05e5e93dcd3
shamil9 <shamil9@gmail.com>
2022-08-17 14:57:49 +0200

    add vs code snippets

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,11 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
 	color_scheme = "nord",
-	font = wezterm.font_with_fallback {
-		'JetBrains Mono',
-	},
-	font = wezterm.font('JetBrains Mono', { weight = 'Medium', italic = false }),
+	font = wezterm.font_with_fallback({
+		"JetBrains Mono",
+	}),
+	font = wezterm.font("JetBrains Mono", { weight = "Medium", italic = false }),
 	font_size = 15.0,
 	line_height = 1.2,
 	use_fancy_tab_bar = false,
