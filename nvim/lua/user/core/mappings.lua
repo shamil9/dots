a62b47468d2ef4b5aecc0a7953cafc1b1ad0c8ef
shamil9 <shamil9@gmail.com>
2022-11-23 17:24:41 +0100

    nvim: update mappings

--- nvim/lua/user/core/mappings.lua
+++ nvim/lua/user/core/mappings.lua
@@ -7,6 +7,15 @@
 return {
   -- first key is the mode
   n = {
+    -- window management
+    ["<C-Left>"] = { function() require("smart-splits").move_cursor_left() end, desc = "Move to left split" },
+    ["<C-Right>"] = { function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" },
+    ["<C-Up>"] = { function() require("smart-splits").move_cursor_up() end, desc = "Move to above split" },
+    ["<C-Down>"] = { function() require("smart-splits").move_cursor_down() end, desc = "Move to below split" },
+    ["<S-Up>"] = { function() require("smart-splits").resize_up() end, desc = "Resize split up" },
+    ["<S-Down>"] = { function() require("smart-splits").resize_down() end, desc = "Resize split down" },
+    ["<S-Left>"] = { function() require("smart-splits").resize_left() end, desc = "Resize split left" },
+    ["<S-Right>"] = { function() require("smart-splits").resize_right() end, desc = "Resize split right" },
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
