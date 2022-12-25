2fc93ed44f7b1121b3c100d968ce9297a0e71e2b
shamil9 <shamil9@gmail.com>
2022-12-25 18:24:02 +0100

    nvim: update mappings & remove unused plugin

--- astro_user/plugins/plugins.lua
+++ astro_user/plugins/plugins.lua
@@ -1,44 +1,40 @@
 return {
-  ["nvim-dap"] = { disable = true },
-  ["mason-nvim-dap.nvim"] = { disable = true },
-  ["nvim-dap-ui"] = { disable = true },
-  { "catppuccin/nvim", as = "catppuccin" },
-  { "EdenEast/nightfox.nvim" },
-  { "tpope/vim-unimpaired" },
-  { "tpope/vim-surround" },
-  {
-    "ggandor/leap.nvim",
-    config = function()
-      require("leap").add_default_mappings(true)
-    end,
-  },
-  --[[ { ]]
-  --[[   "booperlv/nvim-gomove", ]]
-  --[[   config = function() ]]
-  --[[     require("gomove").setup { ]]
-  --[[       map_defaults = false, ]]
-  --[[     } ]]
-  --[[   end, ]]
-  --[[ }, ]]
-  -- You can disable default plugins as follows:
-  -- ["goolord/alpha-nvim"] = { disable = true },
+	{ "catppuccin/nvim", as = "catppuccin" },
+	{ "EdenEast/nightfox.nvim" },
+	{ "tpope/vim-surround" },
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").add_default_mappings(true)
+		end,
+	},
+	--[[ { ]]
+	--[[   "booperlv/nvim-gomove", ]]
+	--[[   config = function() ]]
+	--[[     require("gomove").setup { ]]
+	--[[       map_defaults = false, ]]
+	--[[     } ]]
+	--[[   end, ]]
+	--[[ }, ]]
+	-- You can disable default plugins as follows:
+	-- ["goolord/alpha-nvim"] = { disable = true },
 
-  -- You can also add new plugins here as well:
-  -- Add plugins, the packer syntax without the "use"
-  -- { "andweeb/presence.nvim" },
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function()
-      require("lsp_signature").setup()
-    end,
-  },
+	-- You can also add new plugins here as well:
+	-- Add plugins, the packer syntax without the "use"
+	-- { "andweeb/presence.nvim" },
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "BufRead",
+		config = function()
+			require("lsp_signature").setup()
+		end,
+	},
 
-  -- We also support a key value style plugin definition similar to NvChad:
-  -- ["ray-x/lsp_signature.nvim"] = {
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+	-- We also support a key value style plugin definition similar to NvChad:
+	-- ["ray-x/lsp_signature.nvim"] = {
+	--   event = "BufRead",
+	--   config = function()
+	--     require("lsp_signature").setup()
+	--   end,
+	-- },
 }
