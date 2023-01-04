32e8343ccb78270afcab67cda0e117af23ebbb12
shamil9 <shamil9@gmail.com>
2023-01-04 17:39:33 +0100

    wezterm: update font config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,19 @@
 local wezterm = require("wezterm")
 
 return {
-	color_scheme = "Catppuccin Mocha",
-	color_schemes = {
-		kanagawa = require("colors.kanagawa"),
-	},
-	font = wezterm.font_with_fallback({
-		{ family = "MonoLisa", weight = "Medium", italic = false },
-		{ family = "JetBrains Mono", weight = "Medium", italic = false },
-		"Monaco",
-	}),
-	font_size = 16.0,
-	cell_width = 0.905,
-	line_height = 1.2,
-	use_fancy_tab_bar = false,
-	hide_tab_bar_if_only_one_tab = true,
-	window_decorations = "RESIZE",
+  color_scheme = "Catppuccin Mocha",
+  color_schemes = {
+    kanagawa = require("colors.kanagawa"),
+  },
+  font = wezterm.font_with_fallback({
+    { family = "MonoLisa", weight = "Medium", italic = false },
+    { family = "JetBrains Mono", weight = "Medium", italic = false },
+    "Monaco",
+  }),
+  font_size = 16.0,
+  cell_width = 0.90,
+  line_height = 1.2,
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+  window_decorations = "RESIZE",
 }
