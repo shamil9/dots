d08670b1a198eb68231610d6e508aea28c5298e4
shamil9 <shamil9@gmail.com>
2023-02-10 08:43:25 +0100

    wezterm: update keymap

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -272,81 +272,6 @@ return {
       },
     },
   },
-  keys = {
-    {
-      mods = "ALT",
-      key = [[\]],
-      action = wezterm.action({
-        SplitHorizontal = { domain = "CurrentPaneDomain" },
-      }),
-    },
-    {
-      mods = "ALT|SHIFT",
-      key = [[|]],
-      action = wezterm.action.SplitPane({
-        top_level = true,
-        direction = "Right",
-        size = { Percent = 50 },
-      }),
-    },
-    {
-      mods = "ALT",
-      key = [[-]],
-      action = wezterm.action({
-        SplitVertical = { domain = "CurrentPaneDomain" },
-      }),
-    },
-    {
-      mods = "ALT|SHIFT",
-      key = [[_]],
-      action = wezterm.action.SplitPane({
-        top_level = true,
-        direction = "Down",
-        size = { Percent = 50 },
-      }),
-    },
-    {
-      key = "n",
-      mods = "ALT",
-      action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
-    },
-    {
-      key = "Q",
-      mods = "ALT",
-      action = wezterm.action({ CloseCurrentTab = { confirm = false } }),
-    },
-    { key = "q", mods = "ALT", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
-    { key = "z", mods = "ALT", action = wezterm.action.TogglePaneZoomState },
-    { key = "F11", mods = "", action = wezterm.action.ToggleFullScreen },
-    { key = "h", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
-    { key = "j", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
-    { key = "k", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
-    { key = "l", mods = "ALT|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
-
-    { key = "h", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-left") },
-    { key = "j", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-down") },
-    { key = "k", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-up") },
-    { key = "l", mods = "ALT", action = wezterm.action.EmitEvent("ActivatePaneDirection-right") },
-
-    { key = "[", mods = "ALT", action = wezterm.action({ ActivateTabRelative = -1 }) },
-    { key = "]", mods = "ALT", action = wezterm.action({ ActivateTabRelative = 1 }) },
-    { key = "{", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(-1) },
-    { key = "}", mods = "SHIFT|ALT", action = wezterm.action.MoveTabRelative(1) },
-    { key = "v", mods = "ALT", action = wezterm.action.ActivateCopyMode },
-    { key = "c", mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
-    { key = "v", mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
-    { key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
-    { key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
-    { key = "1", mods = "ALT", action = wezterm.action({ ActivateTab = 0 }) },
-    { key = "2", mods = "ALT", action = wezterm.action({ ActivateTab = 1 }) },
-    { key = "3", mods = "ALT", action = wezterm.action({ ActivateTab = 2 }) },
-    { key = "4", mods = "ALT", action = wezterm.action({ ActivateTab = 3 }) },
-    { key = "5", mods = "ALT", action = wezterm.action({ ActivateTab = 4 }) },
-    { key = "6", mods = "ALT", action = wezterm.action({ ActivateTab = 5 }) },
-    { key = "7", mods = "ALT", action = wezterm.action({ ActivateTab = 6 }) },
-    { key = "8", mods = "ALT", action = wezterm.action({ ActivateTab = 7 }) },
-    { key = "9", mods = "ALT", action = wezterm.action({ ActivateTab = 8 }) },
-  },
   hyperlink_rules = {
     {
       regex = "\\b\\w+://[\\w.-]+:[0-9]{2,15}\\S*\\b",
@@ -371,6 +296,14 @@ return {
     {
       regex = [[\b[tT](\d+)\b]],
       format = "https://example.com/tasks/?t=$1",
+    },
+  },
+  disable_default_mouse_bindings = true,
+  mouse_bindings = {
+    {
+      event = { Up = { streak = 1, button = "Left" } },
+      mods = "NONE",
+      action = wezterm.action.CopyTo("ClipboardAndPrimarySelection"),
     },
   },
 }
