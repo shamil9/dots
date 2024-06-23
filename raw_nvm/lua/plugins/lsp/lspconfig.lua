e89b25820abae29823b50ab1c2429db0c297e5c6
shamil9 <shamil9@gmail.com>
2024-06-23 10:35:44 +0200

    nvim: update lsp signs

--- raw_nvm/lua/plugins/lsp/lspconfig.lua
+++ raw_nvm/lua/plugins/lsp/lspconfig.lua
@@ -2,6 +2,12 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
+		local signs = { Error = " ×", Warn = " ⚠", Hint = "💡", Info = " ℹ" }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+		end
+
 		local lspconfig = require('lspconfig')
 
 		lspconfig.tsserver.setup({})
