9dcabe9a42aff1d7c61e185dc3de7f5726e1775e
shamil9 <shamil9@gmail.com>
2024-06-24 21:04:36 +0200

    nvim: lsp-config ts add fallback server

--- raw_nvm/lua/plugins/lsp/lspconfig.lua
+++ raw_nvm/lua/plugins/lsp/lspconfig.lua
@@ -9,6 +9,26 @@ return {
 		end
 
 		local lspconfig = require('lspconfig')
+		local util = require 'lspconfig.util'
+
+		local function get_typescript_server_path(root_dir)
+			local node_modules = vim.fn.system('npm root -g', true)
+			node_modules = string.gsub(node_modules, "\n", "")
+			local global_ts = node_modules .. '/typescript/lib'
+
+			local found_ts = ''
+			local function check_dir(path)
+				found_ts = util.path.join(path, 'node_modules', 'typescript', 'lib')
+				if util.path.exists(found_ts) then
+					return path
+				end
+			end
+			if util.search_ancestors(root_dir, check_dir) then
+				return found_ts
+			else
+				return global_ts
+			end
+		end
 
 		lspconfig.tsserver.setup({})
 		lspconfig.volar.setup({
@@ -17,7 +37,10 @@ return {
 				vue = {
 					hybridMode = false
 				},
-			}
+			},
+			on_new_config = function(new_config, new_root_dir)
+				new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+			end,
 		})
 		lspconfig.cssls.setup({})
 	end
