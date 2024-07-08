2dc4481360ce321a5c2dd3bfec359010b2a1077a
shamil9 <shamil9@gmail.com>
2024-07-08 22:10:37 +0200

    vscode: update nvim config

--- raw_nvim/vscode/init.lua
+++ raw_nvim/vscode/init.lua
@@ -1,3 +1,19 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
 local vscode = require('vscode')
 
 vim.keymap.set("", "<Space>", "<Nop>")
@@ -7,6 +23,10 @@ vim.keymap.set({ "n", "x", "i" }, "<C-d>", function()
   vscode.with_insert(function()
     vscode.action("editor.action.addSelectionToNextFindMatch")
   end)
+end)
+
+vim.keymap.set({ "n", "x" }, "gr", function()
+  vscode.action("editor.action.goToReferences")
 end)
 
 vim.keymap.set({ "n", "x" }, "<leader>lr", function()
@@ -44,3 +64,18 @@ end)
 vim.keymap.set({ "n", "x" }, "<leader>lh", function()
   vscode.action("editor.action.triggerParameterHints")
 end)
+
+require("lazy").setup({
+  spec = {
+      {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        config = function()
+          require("nvim-surround").setup({})
+        end
+    }
+  },
+  lockfile = vim.fn.stdpath("config") .. "/vscode/lazy-lock.json",
+})
+require("lazy").update({ show = false })
